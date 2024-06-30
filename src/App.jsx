9d4ebe5912b804aac126932671e4058d005f248b
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2024-06-30 23:53:06 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+const App = () => {
+  return (
+   <div>
+    Hello World!
+   </div>
+  );
+};
+
+export default App;
